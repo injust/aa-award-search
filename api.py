9eeee5a4d1c4f5eb1170b57220143378858cb19d
Justin Su <injustsu@gmail.com>
2023-12-11 01:53:16 +0800

    Initial implementation

---
+++ api.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+
+import datetime as dt
+from typing import AsyncIterable, cast
+
+import httpx
+import trio
+from attrs import frozen
+from loguru import logger
+from tenacity import (
+    before_sleep_log,
+    retry,
+    retry_if_exception,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_exponential,
+)
+
+from flights import Availability
+
+
+@frozen
+class Query:
+    origin: str
+    destination: str
+    date: dt.date
+
+    @retry(
+        sleep=trio.sleep,
+        stop=stop_after_attempt(3),
+        wait=wait_exponential(),
+        retry=retry_if_exception_type(httpx.HTTPStatusError)
+        & retry_if_exception(lambda e: cast(httpx.HTTPStatusError, e).response.status_code >= 500),
+        before_sleep=before_sleep_log(logger, "DEBUG"),  # type: ignore[arg-type]
+        reraise=True,
+    )
+    @retry(
+        sleep=trio.sleep,
+        stop=stop_after_attempt(10),
+        wait=wait_exponential(max=32),
+        retry=retry_if_exception_type(httpx.TransportError),
+        before_sleep=before_sleep_log(logger, "DEBUG"),  # type: ignore[arg-type]
+        reraise=True,
+    )
+    async def search(self, httpx_client: httpx.AsyncClient) -> AsyncIterable[Availability]:
+        r = await httpx_client.post(
+            "/search/calendar",
+            json={
+                "metadata": {"selectedProducts": [], "tripType": "OneWay", "udo": {}},
+                "passengers": [{"type": "adult", "count": 1}],
+                "requestHeader": {"clientId": "AAcom"},
+                "slices": [
+                    {
+                        "allCarriers": True,
+                        "cabin": "BUSINESS,FIRST",
+                        "departureDate": self.date.isoformat(),
+                        "destination": self.destination,
+                        "destinationNearbyAirports": False,
+                        "maxStops": None,
+                        "origin": self.origin,
+                        "originNearbyAirports": False,
+                    }
+                ],
+                "tripOptions": {"searchType": "Award", "corporateBooking": False, "locale": "en_US"},
+                "loyaltyInfo": None,
+                "version": "",
+                "queryParams": {"sliceIndex": 0, "sessionId": "", "solutionSet": "", "solutionId": ""},
+            },
+        )
+        r.raise_for_status()
+
+        data = r.json()
+        if (error := data["error"]) and error != "309":
+            raise ValueError(
+                f"Unexpected error code: {error!r}, response_json={data}, request_content={r.request.content.decode()}"
+            )
+
+        if error == 309 and any(
+            day["solution"] for month in data["calendarMonths"] for week in month["weeks"] for day in week["days"]
+        ):
+            logger.warning(f"Error 309 response contains solutions, data={data}")
+
+        for month in data["calendarMonths"]:
+            for week in month["weeks"]:
+                for day in week["days"]:
+                    if solution := day["solution"]:
+                        yield Availability(
+                            dt.date.fromisoformat(day["date"]),
+                            solution["perPassengerAwardPoints"],
+                            Availability.Fees(**solution["perPassengerSaleTotal"]),
+                        )
