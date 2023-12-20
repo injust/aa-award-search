d9eb41195bc73a04936785900b96ed0100e22e71
Justin Su <injustsu@gmail.com>
2023-12-21 02:17:52 +0800

    Improve exception handling and logging

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ import httpx
 import trio
 from attrs import define, field, frozen, validators
 from loguru import logger
+from tenacity import (
+    before_sleep_log,
+    retry,
+    retry_if_exception,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_exponential,
+)
 from trio_typing import TaskStatus
 
 import api
@@ -76,6 +84,18 @@ async def run_job(
 
 
 async def run_task(task: Task, httpx_client: httpx.AsyncClient) -> None:
+    @retry(
+        stop=stop_after_attempt(3),
+        wait=wait_exponential(),
+        retry=retry_if_exception(lambda e: isinstance(e, httpx.HTTPStatusError) and e.response.is_server_error),
+        before_sleep=before_sleep_log(logger, "DEBUG"),  # type: ignore[arg-type]
+    )
+    @retry(
+        stop=stop_after_attempt(10),
+        wait=wait_exponential(max=32),
+        retry=retry_if_exception_type(httpx.TransportError),
+        before_sleep=before_sleep_log(logger, "DEBUG"),  # type: ignore[arg-type]
+    )
     async def run_task_once() -> None:
         try:
             availability = [
@@ -83,14 +103,10 @@ async def run_task(task: Task, httpx_client: httpx.AsyncClient) -> None:
                 async for avail in task.query.search(httpx_client)
                 if all(filter(avail) for filter in task.filters)
             ]
-        except httpx.TransportError as e:
-            logger.warning(f"{e!r}")
-            beep()
         except httpx.HTTPStatusError as e:
-            if not e.response.is_server_error:
-                raise e
-            logger.warning(f"{e!r}, request_content={e.request.content.decode()}")
-            beep()
+            if e.response.is_server_error:
+                logger.debug(f"{e!r}, response_json={e.response.json()}, request_content={e.request.content.decode()}")
+            raise e
         else:
             if (prev_availability := task.availability) is None:
                 logger.info(f"{task.name}\n{pretty_printer().pformat(list(map(Availability.asdict, availability)))}\n")
@@ -144,4 +160,7 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
+    logger.remove()
+    logger.add(sys.stderr, diagnose=True)
+
     trio.run(main)
