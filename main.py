f8e881c4926325e4db919d98e95f36c3f8760a08
Justin Su <injustsu@gmail.com>
2023-12-29 06:51:23 +0900

    Move `httpx_client` to `config.py`

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import datetime as dt
 import random
 import sys
 from collections.abc import Callable, Iterable, Sequence
+from contextlib import aclosing
 from itertools import product
 
 import httpx
@@ -21,7 +22,7 @@ from tenacity import (
 from trio_typing import TaskStatus
 
 import api
-from config import pretty_printer
+from config import httpx_client, pretty_printer
 from flights import Availability
 from utils import beep, compute_diff, format_diff
 
@@ -73,17 +74,15 @@ class Task:
         return f"{self.query.origin}-{self.query.destination}"
 
 
-async def run_job(
-    job: Job, httpx_client: httpx.AsyncClient, *, task_status: TaskStatus[trio.CancelScope] = trio.TASK_STATUS_IGNORED
-) -> None:
+async def run_job(job: Job, *, task_status: TaskStatus[trio.CancelScope] = trio.TASK_STATUS_IGNORED) -> None:
     with trio.CancelScope() as scope:  # pyright: ignore[reportGeneralTypeIssues]
         async with trio.open_nursery() as n:
             for task in job.tasks():
-                n.start_soon(run_task, task, httpx_client)
+                n.start_soon(run_task, task)
             task_status.started(scope)
 
 
-async def run_task(task: Task, httpx_client: httpx.AsyncClient) -> None:
+async def run_task(task: Task) -> None:
     @retry(
         sleep=trio.sleep,
         stop=stop_after_attempt(3),
@@ -101,9 +100,7 @@ async def run_task(task: Task, httpx_client: httpx.AsyncClient) -> None:
     async def run_task_once() -> None:
         try:
             availability = [
-                avail
-                async for avail in task.query.search(httpx_client)
-                if all(filter(avail) for filter in task.filters)
+                avail async for avail in task.query.search() if all(filter(avail) for filter in task.filters)
             ]
         except httpx.HTTPStatusError as e:
             if e.response.is_server_error:
@@ -151,14 +148,9 @@ async def run_task(task: Task, httpx_client: httpx.AsyncClient) -> None:
 async def main() -> None:
     jobs: list[Job] = []
 
-    async with httpx.AsyncClient(
-        http2=True,
-        timeout=httpx.Timeout(5, read=10),
-        limits=httpx.Limits(max_connections=100, max_keepalive_connections=20, keepalive_expiry=60),
-        base_url="https://www.aa.com/booking/api",
-    ) as httpx_client, trio.open_nursery() as n:
+    async with aclosing(httpx_client()), trio.open_nursery() as n:
         for job in jobs:
-            job.scope = await n.start(run_job, job, httpx_client)
+            job.scope = await n.start(run_job, job)
 
 
 if __name__ == "__main__":
