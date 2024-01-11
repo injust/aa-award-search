6b4f709164e9149a4ebef8b4792554dd2e819199
Justin Su <injustsu@gmail.com>
2024-01-11 14:43:17 +0800

    WIP: Add `/search/itinerary` API endpoint

--- flights.py
+++ flights.py
@@ -4,6 +4,9 @@ from typing import override
 from attrs import field, frozen
 from babel.numbers import format_currency
 
+if TYPE_CHECKING:
+    from collections.abc import Iterable, Mapping, Sequence
+
 
 @frozen
 class Availability:
@@ -17,5 +20,63 @@ class Availability:
             return format_currency(self.amount, self.currency)
 
     date: dt.date = field(repr=dt.date.isoformat)
+
     miles: int = field(repr=lambda x: f"{x:_}")
+
     fees: Fees = field(repr=Fees.__str__)
+
+    pricing: Pricing
+
+
+@frozen
+class Itinerary:
+    @frozen
+    class ProductDetail:
+        booking_code: str
+        cabin_type: str
+        product_type: str
+        alerts: Iterable[str]
+
+    @frozen
+    class Route:
+        @frozen
+        class Segment:
+            @frozen
+            class Flight:
+                carrier: str
+                number: int
+
+            alerts: Iterable[str]
+            origin: str
+            destination: str
+            departure: dt.datetime
+            arrival: dt.datetime
+            flight: Flight
+            aircraft: str
+            duration: dt.timedelta
+            connection_time: dt.timedelta
+            product_details: Iterable[Itinerary.ProductDetail]
+
+        origin: str
+        destination: str
+        departure: dt.datetime
+        arrival: dt.datetime
+        stops: int
+        connections: Sequence[str | tuple[str, str]]
+        segments: Sequence[Segment]
+
+    duration: dt.timedelta
+    alerts: Iterable[str]
+    pricing: Mapping[str, Pricing]
+    route: Route
+    product_details: Iterable[Itinerary.ProductDetail]
+
+
+@frozen
+class Pricing:
+    @frozen
+    class Fees:
+        
+
+    miles: int
+    fees: Fees
