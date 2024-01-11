05b26194a655b217c2ec78687882c26641b5dd31
Justin Su <injustsu@gmail.com>
2024-01-11 14:43:17 +0800

    WIP: Add `/search/itinerary` API endpoint

--- flights.py
+++ flights.py
@@ -1,20 +1,18 @@
 from __future__ import annotations
 
 import datetime as dt
+from typing import TYPE_CHECKING
 
 from attrs import Attribute, asdict, frozen
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable, Mapping, Sequence
 
 
 @frozen
 class Availability:
-    @frozen
-    class Fees:
-        amount: float
-        currency: str
-
     date: dt.date
-    miles: int
-    fees: Fees
+    pricing: Pricing
 
     @staticmethod
     def _serialize(_inst: type, _attr: Attribute[object], value: object) -> object:
@@ -26,3 +24,58 @@ class Availability:
 
     def _asdict(self) -> dict[str, object]:
         return asdict(self, value_serializer=self._serialize)
+
+
+@frozen
+class Itinerary:
+    @frozen
+    class ProductDetail:
+        booking_code: str
+        cabin_type: str
+        product_type: str
+        alerts: Iterable[str]
+
+    @frozen
+    class Route:
+        @frozen
+        class Segment:
+            @frozen
+            class Flight:
+                carrier: str
+                number: int
+
+            alerts: Iterable[str]
+            origin: str
+            destination: str
+            departure: dt.datetime
+            arrival: dt.datetime
+            flight: Flight
+            aircraft: str
+            duration: dt.timedelta
+            connection_time: dt.timedelta
+            product_details: Iterable[Itinerary.ProductDetail]
+
+        origin: str
+        destination: str
+        departure: dt.datetime
+        arrival: dt.datetime
+        stops: int
+        connections: Sequence[str | tuple[str, str]]
+        segments: Sequence[Segment]
+
+    duration: dt.timedelta
+    alerts: Iterable[str]
+    pricing: Mapping[str, Pricing]
+    route: Route
+    product_details: Iterable[Itinerary.ProductDetail]
+
+
+@frozen
+class Pricing:
+    @frozen
+    class Fees:
+        amount: float
+        currency: str
+
+    miles: int
+    fees: Fees
