85feafea1be763e909bf015f9938551cd07eaf87
Justin Su <injustsu@gmail.com>
2024-04-20 16:03:47 -0400

    Simplify `config.httpx_client()` since we don't handle closed clients anyway

--- config.py
+++ config.py
@@ -3,23 +3,15 @@ from pprint import PrettyPrinter
 
 import httpx
 
-_httpx_client: httpx.AsyncClient | None = None
 
-
+@cache
 def httpx_client() -> httpx.AsyncClient:
-    global _httpx_client
-
-    if _httpx_client is None:
-        _httpx_client = httpx.AsyncClient(
-            http2=True,
-            timeout=httpx.Timeout(5, read=10),
-            limits=httpx.Limits(max_connections=100, max_keepalive_connections=20, keepalive_expiry=60),
-            base_url="https://www.aa.com/booking/api",
-        )
-    else:
-        assert not _httpx_client.is_closed
-
-    return _httpx_client
+    return httpx.AsyncClient(
+        http2=True,
+        timeout=httpx.Timeout(5, read=10),
+        limits=httpx.Limits(max_connections=100, max_keepalive_connections=20, keepalive_expiry=60),
+        base_url="https://www.aa.com/booking/api",
+    )
 
 
 @cache
