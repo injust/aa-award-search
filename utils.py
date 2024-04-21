1d3f798821df0a2346ab441e08d75847c2c95459
Justin Su <injustsu@gmail.com>
2024-04-21 18:06:26 +0000

    Merge `config.py` into `utils.py`

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
+from functools import cache
+from pprint import PrettyPrinter
 from time import sleep
+
+import httpx
 
 
 def beep(n: int = 1) -> None:
@@ -9,3 +13,18 @@ def beep(n: int = 1) -> None:
     for _ in range(n):
         print("\a", end="", flush=True)
         sleep(INTERVAL)
+
+
+@cache
+def httpx_client() -> httpx.AsyncClient:
+    return httpx.AsyncClient(
+        http2=True,
+        timeout=httpx.Timeout(5, read=10),
+        limits=httpx.Limits(max_connections=100, max_keepalive_connections=20, keepalive_expiry=60),
+        base_url="https://www.aa.com/booking/api",
+    )
+
+
+@cache
+def pretty_printer() -> PrettyPrinter:
+    return PrettyPrinter(width=120, sort_dicts=False, underscore_numbers=True)
