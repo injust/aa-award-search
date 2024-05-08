40321fc270676f4501e9bfa915d03ec9bcbf26b0
Justin Su <injustsu@gmail.com>
2024-05-07 23:14:20 -0400

    Send a real user agent
    Otherwise the request gets reset (HTTP/2) or timed out (HTTP/1.1)

--- utils.py
+++ utils.py
@@ -11,6 +11,9 @@ from httpx._config import DEFAULT_LIMITS
 @cache
 def httpx_client() -> httpx.AsyncClient:
     return httpx.AsyncClient(
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        },
         http2=True,
         timeout=httpx.Timeout(5, read=10),
         limits=httpx.Limits(
