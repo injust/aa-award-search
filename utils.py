8c49f17be517802b1db566b95e17d706621420e3
Justin Su <injustsu@gmail.com>
2024-05-07 23:14:59 -0400

    Set `Accept` header to `application/json`

--- utils.py
+++ utils.py
@@ -22,6 +22,7 @@ def httpx_client() -> httpx.AsyncClient:
     )
     client.headers.update(
         {
+            "Accept": "application/json",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
         }
     )
