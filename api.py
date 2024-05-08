d2378018afda800a1f14588963a66c785f21898e
Justin Su <injustsu@gmail.com>
2024-05-08 04:21:57 -0400

    TEST: Add support for searching with maximum number of stops
    NOTE: AA's API seems to return incorrect results when sending certain `maxStops` parameters.

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ from typing import Any, override
 
 import orjson as jsonlib
 from attrs import field, frozen
-from attrs.validators import ge, le
+from attrs.validators import ge, le, optional
 from loguru import logger
 
 from flights import Availability
@@ -20,6 +20,7 @@ class Query(ABC):
     origin: str
     destination: str
     date: dt.date
+    max_stops: int | None = field(default=None, validator=optional([ge(0), le(3)]))
     passengers: int = field(default=1, validator=[ge(1), le(9)])
 
     async def _send_query(self, endpoint: str) -> dict[str, Any]:
@@ -34,7 +35,7 @@ class Query(ABC):
                     "departureDate": self.date.isoformat(),
                     "destination": self.destination,
                     "destinationNearbyAirports": False,
-                    "maxStops": None,
+                    "maxStops": self.max_stops,
                     "origin": self.origin,
                     "originNearbyAirports": False,
                 }
