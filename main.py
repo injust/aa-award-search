57266827ac3b277673f7e2362314f504c155c3b1
Justin Su <injustsu@gmail.com>
2024-05-08 04:21:57 -0400

    TEST: Add support for searching with maximum number of stops
    NOTE: AA's API seems to return incorrect results when sending certain `maxStops` parameters.

--- main.py
+++ main.py
@@ -96,6 +96,7 @@ class Job:
         origins: Collection[str] = field(validator=[min_len(1), not_(instance_of(str))])
         destinations: Collection[str] = field(validator=[min_len(1), not_(instance_of(str))])
         dates: QueryRange
+        max_stops: int | None = None
         passengers: int = 1
 
     query: Query
@@ -113,7 +114,10 @@ class Job:
 
             yield Task(
                 f"{self.label or ''} {origin}-{destination} {dates[0]:%Y-%m}".lstrip(),
-                [CalendarQuery(origin, destination, date, self.query.passengers) for date in dates],
+                [
+                    CalendarQuery(origin, destination, date, self.query.max_stops, self.query.passengers)
+                    for date in dates
+                ],
                 self.frequency,
                 [*self.filters, date_in_range],
             )
@@ -128,7 +132,7 @@ class Job:
 
             yield Task(
                 f"{self.label or ''} {origin}-{destination} {date}".lstrip(),
-                [WeeklyQuery(origin, destination, date, self.query.passengers)],
+                [WeeklyQuery(origin, destination, date, self.query.max_stops, self.query.passengers)],
                 self.frequency,
                 [*self.filters, date_in_range],
             )
