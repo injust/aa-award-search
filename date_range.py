1fe622ca0e5a097f0baba7dce14d4205a701102a
Justin Su <injustsu@gmail.com>
2024-08-15 16:50:10 -0400

    Replace `strftime` with f-string formatting if already in an f-string

--- date_range.py
+++ date_range.py
@@ -57,7 +57,7 @@ class DayRange:
 
     @override
     def __str__(self) -> str:
-        return f"{self.start.strftime("%Y/%m/%d")}-{self.stop.strftime("%Y/%m/%d")}"
+        return f"{self.start:%Y/%m/%d)}-{self.stop:%Y/%m/%d)}"
 
 
 @frozen
@@ -108,4 +108,4 @@ class MonthRange:
 
     @override
     def __str__(self) -> str:
-        return f"{self.start.strftime("%Y/%m")}-{self.stop.strftime("%Y/%m")}"
+        return f"{self.start:%Y/%m)}-{self.stop:%Y/%m)}"
