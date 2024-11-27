23a520e24961a0374877ee21be5311a7e5909132
Justin Su <injustsu@gmail.com>
2024-11-27 16:16:50 -0500

    Use orjson

--- utils.py
+++ utils.py
@@ -13,6 +13,7 @@ def httpx_client() -> httpx.AsyncClient:
     return httpx.AsyncClient(
         headers={
             "Accept": "application/json",
+            "Content-Type": "application/json",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
         },
         http2=True,
