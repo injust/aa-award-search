87c67eed435cb621669c90d9b8a5b1c36c6e5aa9
Justin Su <injustsu@gmail.com>
2025-01-09 03:15:12 -0500

    Improve return type annotation on generators

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ from flights import Availability
 from utils import httpx_client
 
 if TYPE_CHECKING:
-    from collections.abc import AsyncIterable
+    from collections.abc import AsyncGenerator, AsyncIterable
 
 
 @frozen
@@ -77,7 +77,7 @@ class AvailabilityQuery(Query, ABC):
 @frozen
 class CalendarQuery(AvailabilityQuery):
     @override
-    async def search(self) -> AsyncIterable[Availability]:
+    async def search(self) -> AsyncGenerator[Availability]:
         data = await self._send_query("search/calendar")
 
         if data["error"] == 309 and any(
@@ -99,7 +99,7 @@ class CalendarQuery(AvailabilityQuery):
 @frozen
 class WeeklyQuery(AvailabilityQuery):
     @override
-    async def search(self) -> AsyncIterable[Availability]:
+    async def search(self) -> AsyncGenerator[Availability]:
         data = await self._send_query("search/weekly")
 
         if data["error"] == 309 and any(day["solutionId"] for day in data["days"]):
