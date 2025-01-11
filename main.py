cfc82d7d9cd96ac4f54cd24f40a078037e09d54b
Justin Su <injustsu@gmail.com>
2025-01-11 03:15:00 -0500

    `QueryRange.step` is guaranteed to be non-zero by `DayRange`

--- main.py
+++ main.py
@@ -73,7 +73,8 @@ class Job:
             SEARCH_WIDTH: ClassVar[dt.timedelta] = SEARCH_RADIUS * 2 + dt.timedelta(days=1)
 
             def __attrs_post_init__(self) -> None:
-                if self.step <= dt.timedelta():
+                assert self.step
+                if self.step < dt.timedelta():
                     raise ValueError("`step` must be positive")
                 elif not bool(self):
                     raise ValueError("`QueryRange` must be a non-empty range")
