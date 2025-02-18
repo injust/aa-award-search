f5dadd4faa8b8d6ebb4704bdc44dcf836daa8fb1
Justin Su <injustsu@gmail.com>
2025-02-18 10:31:54 -0500

    Remove `httpx.HTTPStatusError` info suffix from `raise_for_status()`

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ from tenacity import (
 from api import CalendarQuery, WeeklyQuery
 from date_range import DayRange, MonthRange
 from flights import Availability
-from utils import beep, httpx_client, pretty_printer
+from utils import beep, httpx_client, httpx_remove_HTTPStatusError_info_suffix, pretty_printer
 
 if TYPE_CHECKING:
     from collections.abc import Callable, Collection, Generator, Iterable
@@ -34,6 +34,8 @@ if TYPE_CHECKING:
     from api import AvailabilityQuery
 
     type DiffLine = tuple[Literal[" ", "+", "-"], Availability]
+
+httpx.Response.raise_for_status = httpx_remove_HTTPStatusError_info_suffix(httpx.Response.raise_for_status)  # type: ignore[assignment, method-assign]  # pyright: ignore[reportAttributeAccessIssue]
 
 
 @frozen
