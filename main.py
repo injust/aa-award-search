5d6ece042fa84d6c82da77575a80b937fa0af049
Justin Su <injustsu@gmail.com>
2025-04-09 20:16:56 -0400

    Use `httpx_response_jsonlib()`

--- main.py
+++ main.py
@@ -26,13 +26,14 @@ from tenacity import (
 from api import AvailabilityQuery, CalendarQuery, WeeklyQuery
 from date_range import DayRange, MonthRange
 from flights import Availability
-from utils import beep, httpx_client, httpx_remove_HTTPStatusError_info_suffix, pretty_printer
+from utils import beep, httpx_client, httpx_remove_HTTPStatusError_info_suffix, httpx_response_jsonlib, pretty_printer
 
 if TYPE_CHECKING:
     from collections.abc import Callable, Collection, Generator, Iterable
 
     type DiffLine = tuple[Literal[" ", "+", "-"], Availability]
 
+httpx.Response.json = httpx_response_jsonlib  # type: ignore[method-assign]
 httpx.Response.raise_for_status = httpx_remove_HTTPStatusError_info_suffix(httpx.Response.raise_for_status)  # type: ignore[assignment, method-assign]  # pyright: ignore[reportAttributeAccessIssue]
 
 
