f63ccb6dc1a00740e8d4c34d1b22a8fd45d10648
Justin Su <injustsu@gmail.com>
2025-05-31 21:34:01 -0400

    Turn cached `httpx_client` and `pretty_printer` into variables

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ class Diff:
         LOGURU_COLOR_TAGS = {" ": "dim", "+": "green", "-": "red"}
 
         return "\n".join(
-            f"<{LOGURU_COLOR_TAGS[change]}>{change}{pretty_printer().pformat(avail._asdict())}</>"
+            f"<{LOGURU_COLOR_TAGS[change]}>{change}{pretty_printer.pformat(avail._asdict())}</>"
             for change, avail in self.lines
         )
 
@@ -179,7 +179,7 @@ class Task:
         if self.frequency is None:
             availability = await run_once()
 
-            logger.info("{}\n{}\n", self.name, pretty_printer().pformat(list(map(Availability._asdict, availability))))
+            logger.info("{}\n{}\n", self.name, pretty_printer.pformat(list(map(Availability._asdict, availability))))
             if availability:
                 beep()
 
@@ -206,7 +206,7 @@ class Task:
             else:
                 if prev_availability is None:
                     logger.info(
-                        "{}\n{}\n", self.name, pretty_printer().pformat(list(map(Availability._asdict, availability)))
+                        "{}\n{}\n", self.name, pretty_printer.pformat(list(map(Availability._asdict, availability)))
                     )
                     if availability:
                         beep()
@@ -226,7 +226,7 @@ async def main() -> None:
     jobs: list[Job] = []
 
     try:
-        async with httpx_client(), create_task_group() as tg:
+        async with httpx_client, create_task_group() as tg:
             for job in jobs:
                 tg.start_soon(job.run)
     except* (CancelledError, KeyboardInterrupt):
