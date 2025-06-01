6c5d53d9cf6379800900329d399c0248b4c74ea4
Justin Su <injustsu@gmail.com>
2025-05-31 21:34:01 -0400

    Turn cached `httpx_client` and `pretty_printer` into variables

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import time
-from functools import cache, wraps
+from functools import wraps
 from pprint import PrettyPrinter
 from typing import TYPE_CHECKING, Any
 
@@ -40,29 +40,22 @@ def beep(times: int = 1, *, interval: float = 0.15) -> None:
         time.sleep(interval)
 
 
-@cache
-def httpx_client() -> httpx.AsyncClient:
-    return httpx.AsyncClient(
-        headers={
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
-        },
-        http2=True,
-        timeout=httpx.Timeout(5, read=10),
-        limits=httpx.Limits(
-            max_connections=DEFAULT_LIMITS.max_connections,
-            max_keepalive_connections=DEFAULT_LIMITS.max_keepalive_connections,
-            keepalive_expiry=60,
-        ),
-        base_url="https://www.aa.com/booking/api/",
-    )
-
-
-@cache
-def pretty_printer() -> PrettyPrinter:
-    return PrettyPrinter(width=120, sort_dicts=False, underscore_numbers=True)
-
-
 httpx.Response.json = httpx_response_jsonlib  # type: ignore[method-assign]
 httpx.Response.raise_for_status = httpx_remove_HTTPStatusError_info_suffix(httpx.Response.raise_for_status)  # type: ignore[assignment, method-assign]  # pyright: ignore[reportAttributeAccessIssue]
+httpx_client = httpx.AsyncClient(
+    headers={
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+    },
+    http2=True,
+    timeout=httpx.Timeout(5, read=10),
+    limits=httpx.Limits(
+        max_connections=DEFAULT_LIMITS.max_connections,
+        max_keepalive_connections=DEFAULT_LIMITS.max_keepalive_connections,
+        keepalive_expiry=60,
+    ),
+    base_url="https://www.aa.com/booking/api/",
+)
+
+pretty_printer = PrettyPrinter(width=120, sort_dicts=False, underscore_numbers=True)
